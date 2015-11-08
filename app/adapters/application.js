4c441017954618ed6d5d99041c2d6634b82fabd9
Manu S Ajith <neo@codingarena.in>
2015-11-08 06:40:48 +0530

    Fix typo and endpoints
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,14 +1,14 @@
-import config from '../config/enviornment';
+import config from '../config/environment';
 import FixtureAdapter from 'ember-data-fixture-adapter';
-import DS from 'ember-data'
+import DS from 'ember-data';
 
 var adapter;
-if (config.eviornment === 'test') {
+if (config.environment === 'test') {
   adapter = FixtureAdapter.extend({});
 } else {
   adapter = DS.RESTAdapter.extend({
-    host: 'http:localhost:4000/api'
-  })
+    host: 'http://localhost:4000/api'
+  });
 }
 
 export default adapter;
